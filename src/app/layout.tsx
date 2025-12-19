8d9083695c68fcf5ac1460df86019890d1ad70d5
real-zephex <zephex-1@outlook.com>
2025-12-20 01:57:50 +0530

    removed animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { kanit } from "@/fonts/fonts";
-import AnimatedBackground from "@/components/ui/animated-background";
 
 export const metadata: Metadata = {
   title: "Sumit's Portfolio",
   description: "The official portfolio of Sumit Kumar.",
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
-  
 };
 
 export default function RootLayout({
@@ -18,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="lg:cursor-none">
       <body className={`${kanit.className} antialiased `} data-theme="dark">
-        <AnimatedBackground />
         {children}
       </body>
     </html>
